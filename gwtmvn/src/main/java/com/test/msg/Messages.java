2d2d3ad71e1fd9cf80f9ba12bf6db00da9e42556
zhilw <zhilw@cenboomh.com>
2017-08-02 13:10:47 +0800

    提交gwt maven测试工程

---
+++ gwtmvn/src/main/java/com/test/msg/Messages.java
@@ -0,0 +1,26 @@
+package com.test.msg;
+
+/**
+ * Interface to represent the messages contained in resource bundle:
+ * 	C:/work/code/workspace/his-ws/gwtmvn/src/main/resources/com/test/Messages.properties'.
+ */
+public interface Messages extends com.google.gwt.i18n.client.Messages {
+  
+  /**
+   * Translated "Enter your name".
+   * 
+   * @return translated "Enter your name"
+   */
+  @DefaultMessage("Enter your name")
+  @Key("nameField")
+  String nameField();
+
+  /**
+   * Translated "Send".
+   * 
+   * @return translated "Send"
+   */
+  @DefaultMessage("Send")
+  @Key("sendButton")
+  String sendButton();
+}
